b1378566da29c714f374ee763e63cc68d2c0fe1f
NAD <lua.solution>
2021-07-13 14:28:08 +0700

    c

--- src/views/user/news.js
+++ src/views/user/news.js
@@ -16,7 +16,7 @@ const { Paragraph } = Typography
 const pageSize = 8
 const rand = Math.random()
 
-const NewsPage = props => {
+const NewsPage = (props) => {
   const [data, setData] = useState([])
   const [loading, setLoading] = useState(false)
   const [visible, setVisible] = useState(false)
@@ -24,7 +24,7 @@ const NewsPage = props => {
   const [loadingDetail, setLoadingDetail] = useState(false)
 
   useEffect(() => {
-    const editId = new URLSearchParams(props.location.search).get("p")
+    const editId = new URLSearchParams(props.location.search).get('p')
     if (editId) {
       onOpenDrawer(editId)
     }
@@ -32,28 +32,30 @@ const NewsPage = props => {
 
   const fetchData = () => {
     setLoading(true)
-    axios.get('tin-tuc/paging/' + Math.ceil(data.length / pageSize))
-      .then(res => {
+    axios
+      .get('tin-tuc/paging/' + data.length)
+      .then((res) => {
         if (res.data.status === 200) {
-          setData(res.data.data)
+          setData([...data, ...res.data.data])
         }
       })
       .finally(() => setLoading(false))
-      .catch(err => console.log(err))
+      .catch((err) => console.log(err))
   }
 
   useEffect(() => {
     fetchData()
   }, [])
 
-  const onOpenDrawer = id => {
+  const onOpenDrawer = (id) => {
     setVisible(true)
     setLoadingDetail(true)
-    axios.get('tin-tuc/get-info/' + id)
-      .then(res => {
+    axios
+      .get('tin-tuc/get-info/' + id)
+      .then((res) => {
         return res.data
       })
-      .then(data => {
+      .then((data) => {
         setDetail(data)
       })
       .finally(() => setLoadingDetail(false))
@@ -64,90 +66,124 @@ const NewsPage = props => {
     setVisible(false)
   }
 
-  const renderDetail = item => {
-    return <Card>
-      <CardBody>
-        <div style={{
-          borderRadius: 5,
-          width: '100%',
-          height: 300,
-          backgroundImage: `url(${NEWS_PATH + item.id + '.webp?' + rand})`,
-          backgroundSize: 'cover',
-          backgroundRepeat: 'no-repeat',
-          backgroundPosition: 'center',
-          marginBottom: 15
-        }}></div>
-        <h3 style={{ textAlign: 'center', fontWeight: 'bold', margin: '50px 0' }}>{item.title}</h3>
-        <div dangerouslySetInnerHTML={{ __html: item.content }} style={{ margin: 0 }} />
-      </CardBody>
-    </Card>
+  const renderDetail = (item) => {
+    return (
+      <Card>
+        <CardBody>
+          <div
+            style={{
+              borderRadius: 5,
+              width: '100%',
+              height: 300,
+              backgroundImage: `url(${NEWS_PATH + item.id + '.webp?' + rand})`,
+              backgroundSize: 'cover',
+              backgroundRepeat: 'no-repeat',
+              backgroundPosition: 'center',
+              marginBottom: 15,
+            }}
+          ></div>
+          <h3
+            style={{
+              textAlign: 'center',
+              fontWeight: 'bold',
+              margin: '50px 0',
+            }}
+          >
+            {item.title}
+          </h3>
+          <div
+            dangerouslySetInnerHTML={{ __html: item.content }}
+            style={{ margin: 0 }}
+          />
+        </CardBody>
+      </Card>
+    )
   }
 
-  return <>
-    <UserHeaderLayout />
-    <div className="ifa-body-wrapper">
-      <div className="ifa-container ifa-news-elearning" style={{ backgroundImage: `url(${bgNewsElearning})`, marginTop: 0, paddingTop: 50 }}>
-        <div className="container ifa-container ifa-news-block">
-          <div className="ifa-block-content container">
-            <p className="top-small-text">
-              Liên tục những hoạt động sôi nổi
-            </p>
-            <p className="top-lg-text">
-              Tin tức - Sự kiện
-            </p>
-            <Spin spinning={loading}>
-              <div className="row list-news">
-                {data && data.map((item, index) =>
-                  <div className="col-lg-4 col-md-12" key={index} style={{ marginBottom: 15 }}>
-                    <div className="item ifa-item" style={{ border: '1px solid #eee' }}>
-                      <a href={'/news?p=' + item.id}>
-                        <div className="img" style={{
-                          borderTopLeftRadius: 5,
-                          borderTopRightRadius: 5,
-                          height: 200,
-                          backgroundImage: `url(${NEWS_PATH + item.id + '.webp?' + rand})`,
-                          backgroundSize: 'cover',
-                          backgroundRepeat: 'no-repeat'
-                        }}>
+  return (
+    <>
+      <UserHeaderLayout />
+      <div className="ifa-body-wrapper">
+        <div
+          className="ifa-container ifa-news-elearning"
+          style={{
+            backgroundImage: `url(${bgNewsElearning})`,
+            marginTop: 0,
+            paddingTop: 50,
+          }}
+        >
+          <div className="container ifa-container ifa-news-block">
+            <div className="ifa-block-content container">
+              <p className="top-small-text">Liên tục những hoạt động sôi nổi</p>
+              <p className="top-lg-text">Tin tức - Sự kiện</p>
+              <Spin spinning={loading}>
+                <div className="row list-news">
+                  {data &&
+                    data.map((item, index) => (
+                      <div
+                        className="col-lg-4 col-md-12"
+                        key={index}
+                        style={{ marginBottom: 15 }}
+                      >
+                        <div
+                          className="item ifa-item"
+                          style={{ border: '1px solid #eee' }}
+                        >
+                          <a href={'/news?p=' + item.id}>
+                            <div
+                              className="img"
+                              style={{
+                                borderTopLeftRadius: 5,
+                                borderTopRightRadius: 5,
+                                height: 200,
+                                backgroundImage: `url(${
+                                  NEWS_PATH + item.id + '.webp?' + rand
+                                })`,
+                                backgroundSize: 'cover',
+                                backgroundRepeat: 'no-repeat',
+                              }}
+                            ></div>
+                            <div className="content">
+                              <div className="status-wrapper">
+                                <span className="pos">Đã đăng | </span>
+                                <span className="status comming">
+                                  {momentTime(item.created_at)}
+                                </span>
+                              </div>
+                              <div className="title">
+                                <Paragraph ellipsis={{ rows: 3 }}>
+                                  {item.title}
+                                </Paragraph>
+                              </div>
+                              <div className="description">{item.s_des}</div>
+                            </div>
+                          </a>
                         </div>
-                        <div className="content">
-                          <div className="status-wrapper">
-                            <span className="pos">Đã đăng | </span>
-                            <span className="status comming">{momentTime(item.created_at)}</span>
-                          </div>
-                          <div className="title">
-                            <Paragraph ellipsis={{ rows: 3 }} >
-                              {item.title}
-                            </Paragraph>
-                          </div>
-                          <div className="description">{item.s_des}</div>
-                        </div>
-                      </a>
-                    </div>
-                  </div>)}
-              </div>
-            </Spin>
-            {!loading && <div className='btn-load-more-expert' style={{ marginTop: 40 }}>
-              <button onClick={fetchData}>Xem thêm</button>
-            </div>}
+                      </div>
+                    ))}
+                </div>
+              </Spin>
+              {!loading && (
+                <div className="btn-load-more-expert" style={{ marginTop: 40 }}>
+                  <button onClick={fetchData}>Xem thêm</button>
+                </div>
+              )}
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    <Drawer
-      width={'100%'}
-      placement="right"
-      closable={true}
-      onClose={onCloseDrawer}
-      visible={visible}
-
-    >
-      {loadingDetail ? <Skeleton />
-        :
-        detail ? renderDetail(detail) : null}
-    </Drawer>
-    <UserFooterLayout />
-  </>
+      <Drawer
+        width={'100%'}
+        placement="right"
+        closable={true}
+        onClose={onCloseDrawer}
+        visible={visible}
+      >
+        {loadingDetail ? <Skeleton /> : detail ? renderDetail(detail) : null}
+      </Drawer>
+      <UserFooterLayout />
+    </>
+  )
 }
 
 export default NewsPage
